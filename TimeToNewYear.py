fe5044b6a185ab5ba068c68c6fd7cd524876607d
Luiz Fernando Nunes <luiz_fernando_nunes@hotmail.com>
2017-12-30 19:26:51 -0200

    Add main file
    Criação do arquivo principal

---
+++ TimeToNewYear.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import datetime, re
+
+class NewYear:
+    def __init__(self):
+        self.window = Tk()
+        x = (self.window.winfo_screenwidth() / 2) - (400 / 2)
+        y = (self.window.winfo_screenheight() / 2) - (500 / 2)
+        self.window.title("Contador do Ano Novo")
+        self.window.resizable(False,False)
+        self.create_widgets()
+        self.month = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho",
+                      "Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+        self.window.after(10,self.update)
+        self.window.mainloop()
+    
+    def create_widgets(self):
+        self.labelCounter = Label(self.window, text="00:00:00", justify=CENTER, font=("Arial",100))
+        self.labelCounter.grid(row=0,column=0)
+        self.labelDate = Label(self.window,text="Hoje é DIA de MÊS de ANO", justify=LEFT, font=("Arial",18))
+        self.labelDate.grid(row=1,column=0)
+
+
+    def update(self):
+        self.labelCounter['text'] = self.calcTime()
+        self.labelDate["text"] = self.show_day()
+        self.window.after(1000, self.update)
+
+    def calcTime(self):
+        today = datetime.datetime.now()
+        year = today.year + 1
+        new_year = datetime.datetime(year,1,1)
+        regressive = new_year - today
+        text = str(regressive).split(".")[0]
+        new = re.sub("day[s]?","dias",text)
+        if re.search("^1",new) != None:
+            new = re.sub("dias","dia",new)
+        return new
+
+    def show_day(self):
+        today = datetime.datetime.now()
+        return "Hoje é {} de {} de {}".format(today.day, self.month[today.month - 1], today.year)
+
+if __name__=="__main__":
+    App = NewYear()
